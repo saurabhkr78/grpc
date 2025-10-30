a2a015b39256091116e0acd93ab0911258a08167
saurabhkr78 <meetsaurabh78@gmail.com>
2025-10-30 09:27:25 +0000

    learning grpc implementation

---
+++ client/client.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "grpc/proto"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	//connect to server
+	//It sets up: the TCP connection,any transport security (TLS/SSL),and connection-level options.
+
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewHelloServiceClient(conn)
+
+	//make rpc call
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	resp, err := client.SayHello(ctx, &pb.HelloRequest{Name: "Saurabh"})
+	if err != nil {
+		log.Fatalf("Error calling SayHello: %v", err)
+	}
+
+	log.Printf("Response from server: %v", resp.Message)
+}
---
+++ server/server.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "grpc/proto"
+
+	"google.golang.org/grpc"
+)
+
+// server is used to implement hello_grpc.HelloServiceServer.
+type server struct {
+	pb.UnimplementedHelloServiceServer
+}
+
+func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
+	log.Printf("Hello %s! Welcome to gRPC services.\n", req.Name)
+	return &pb.HelloResponse{Message: "Hello " + req.Name + "! Welcome to gRPC services."}, nil
+
+}
+func main() {
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterHelloServiceServer(grpcServer, &server{})
+	log.Println("gRPC server is running on port :50051")
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
