4e1aabd133de3066600ca99183de7a3ce2ef09fe
saurabhkr78 <meetsaurabh78@gmail.com>
2025-10-30 09:29:41 +0000

    fix:removed depracated method to create a client

--- client/client.go
+++ client/client.go
@@ -8,13 +8,14 @@ import (
 	pb "grpc/proto"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
 	//connect to server
 	//It sets up: the TCP connection,any transport security (TLS/SSL),and connection-level options.
 
-	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("Could not connect: %v", err)
 	}
