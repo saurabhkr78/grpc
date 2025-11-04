8ee5609297e5d062ebb4f9cd791566e5a571d36a
saurabhkr78 <meetsaurabh78@gmail.com>
2025-11-04 23:19:07 +0000

    implmented all the types of gRPC methods in go lang

--- client/client.go
+++ client/client.go
@@ -2,35 +2,85 @@ package main
 
 import (
 	"context"
+	"io"
 	"log"
 	"time"
 
-	pb "grpc/proto"
+	pb "grpc/grpc/gen"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
-	//connect to server
-	//It sets up: the TCP connection,any transport security (TLS/SSL),and connection-level options.
-
 	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Fatalf("Could not connect: %v", err)
+		log.Fatalf("Failed to connect: %v", err)
 	}
 	defer conn.Close()
 
-	client := pb.NewHelloServiceClient(conn)
+	client := pb.NewUserServiceClient(conn)
 
-	//make rpc call
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
-	resp, err := client.SayHello(ctx, &pb.HelloRequest{Name: "Saurabh is working on grpc"})
+	// --- Unary RPC ---
+	log.Println("Unary RPC: GetUser()")
+	user, err := client.GetUser(context.Background(), &pb.GetUserRequest{UserId: 1})
 	if err != nil {
-		log.Fatalf("Error calling SayHello: %v", err)
+		log.Fatalf("Error in GetUser: %v", err)
+	}
+	log.Printf("Response: %+v\n", user.User)
+
+	// --- Server Streaming RPC ---
+	log.Println("Server Streaming: GetUsersStream()")
+	stream1, _ := client.GetUsersStream(context.Background(), &pb.GetUsersRequest{UserIds: []string{"101", "102", "103"}})
+	for {
+		res, err := stream1.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error receiving stream: %v", err)
+		}
+		log.Printf("Received user: %+v\n", res)
 	}
 
-	log.Printf("Response from server: %v", resp.Message)
+	// --- Client Streaming RPC ---
+	log.Println("Client Streaming: CreateUsersStream()")
+	stream2, _ := client.CreateUsersStream(context.Background())
+	for i := 1; i <= 30; i++ {
+		stream2.Send(&pb.CreateUserRequest{
+			Name:  "User_" + time.Now().Format("150405"),
+			Email: "temp@example.com",
+		})
+		time.Sleep(500 * time.Millisecond)
+	}
+	resp, _ := stream2.CloseAndRecv()
+	log.Printf("Users Created: %d | IDs: %v", resp.UsersCreated, resp.UserIds)
+
+	// --- Bidirectional Streaming RPC ---
+	log.Println("Bidirectional Streaming: Chat()")
+	stream3, _ := client.Chat(context.Background())
+
+	go func() {
+		for i := 1; i <= 3; i++ {
+			msg := &pb.ChatMessage{
+				UserId:    "client_1",
+				Message:   "Hello Server!",
+				Timestamp: time.Now().Unix(),
+			}
+			stream3.Send(msg)
+			time.Sleep(time.Second)
+		}
+		stream3.CloseSend()
+	}()
+
+	for {
+		in, err := stream3.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error in chat stream: %v", err)
+		}
+		log.Printf("Server: %s", in.Message)
+	}
 }
--- server/server.go
+++ server/server.go
@@ -2,36 +2,116 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io"
 	"log"
 	"net"
-
-	pb "grpc/proto"
+	"time"
 
 	"google.golang.org/grpc"
+	pb "grpc/grpc/gen"
 )
 
-// server is used to implement hello_grpc.HelloServiceServer.
-type server struct {
-	pb.UnimplementedHelloServiceServer
+// Implementing the UserServiceServer interface
+type UserServer struct {
+	pb.UnimplementedUserServiceServer
+}
+
+// Unary RPC
+func (s *UserServer) GetUser(ctx context.Context, req *pb.GetUserRequest) (*pb.GetUserResponse, error) {
+	log.Printf("Received GetUser request for user_id: %d", req.GetUserId())
+
+	user := &pb.UserResponse{
+		Id:       fmt.Sprintf("%d", req.GetUserId()),
+		Name:     fmt.Sprintf("User_%d", req.GetUserId()),
+		Email:    fmt.Sprintf("user_%d@example.com", req.GetUserId()),
+		IsActive: true,
+	}
+
+	return &pb.GetUserResponse{User: user}, nil
+}
+
+// Server Streaming RPC
+func (s *UserServer) GetUsersStream(req *pb.GetUsersRequest, stream pb.UserService_GetUsersStreamServer) error {
+	for _, id := range req.UserIds {
+		res := &pb.UserResponse{
+			Id:       id,
+			Name:     fmt.Sprintf("User_%s", id),
+			Email:    fmt.Sprintf("user%s@example.com", id),
+			IsActive: true,
+		}
+
+		if err := stream.Send(res); err != nil {
+			return err
+		}
+
+		log.Printf("Sent user: %s", id)
+		time.Sleep(time.Second)
+	}
+	return nil
+}
+
+// Client Streaming RPC
+func (s *UserServer) CreateUsersStream(stream pb.UserService_CreateUsersStreamServer) error {
+	var count int32
+	var userIDs []string
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.CreateUsersResponse{
+				UsersCreated: count,
+				UserIds:      userIDs,
+			})
+		}
+		if err != nil {
+			return err
+		}
+
+		count++
+		newID := fmt.Sprintf("%d", time.Now().UnixNano())
+		userIDs = append(userIDs, newID)
+		log.Printf("Created user: %s (%s)", req.Name, req.Email)
+	}
+}
+
+// Bidirectional Streaming RPC
+func (s *UserServer) Chat(stream pb.UserService_ChatServer) error {
+	for {
+		msg, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		log.Printf("[%s]: %s", msg.UserId, msg.Message)
+
+		resp := &pb.ChatMessage{
+			UserId:    "Server",
+			Message:   "Received: " + msg.Message,
+			Timestamp: time.Now().Unix(),
+		}
+
+		if err := stream.Send(resp); err != nil {
+			return err
+		}
+	}
 }
 
 func main() {
-	listener, err := net.Listen("tcp", ":50051")
+	port := ":50051"
+	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 
-	grpcServer := grpc.NewServer()
+	s := grpc.NewServer()
+	pb.RegisterUserServiceServer(s, &UserServer{})
 
-	pb.RegisterHelloServiceServer(grpcServer, &server{})
-	log.Println("gRPC server is running on port :50051")
-	if err := grpcServer.Serve(listener); err != nil {
+	log.Printf("gRPC Server running on port %s", port)
+	if err := s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
 	}
-}
-func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
-	log.Println("Received  request from client")
-	log.Printf("Hello %s! Welcome to gRPC server.\n", req.Name)
-	return &pb.HelloResponse{Message: "Hello " + req.Name + "! Welcome to gRPC services."}, nil
-
 }
